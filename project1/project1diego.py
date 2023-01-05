28bb960f326b3a1a1de39bb3bdb8d9743e6e5559
Diego_sic <diego.sic@centre.edu>
2023-01-04 21:26:03 -0500

    initial commit

---
+++ project1/project1diego.py
@@ -0,0 +1,46 @@
+import random
+
+def simulate_election(num_voters, num_candidates):
+    preferences = []
+
+    for i in range(num_voters):
+        #generate random voters preferences
+        preferences.append(random.sample(range(num_candidates), num_candidates))
+    
+    return preferences
+
+def plurality(num_voters, num_candidates):
+    ballots = simulate_election(num_voters, num_candidates)
+    plurality = []
+    for ballot in ballots:
+        plurality.append(ballot[0])
+    return  max(set(plurality), key = plurality.count)
+
+def condercet(num_voters, num_candidates):
+    ballots = simulate_election(num_voters, num_candidates)
+    absolute_winner = 0
+    for candidate in range(1,num_candidates):
+        votes_for_aw = 0
+        votes_for_candidate = 0
+        for ballot in ballots:
+            aw = ballot.index(absolute_winner)
+            c = ballot.index(candidate)
+            if aw < c:
+                votes_for_aw += 1
+            else:
+                votes_for_candidate += 0
+            
+        
+
+
+
+def main():
+    #Plurality section
+    num_voters = 10
+    num_candidates = 3
+    condercet(num_voters, num_candidates)
+
+    
+    
+if __name__ == "__main__":
+    main()
