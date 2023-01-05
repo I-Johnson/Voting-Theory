3cddeb814841b03f3bd310b74395758b427281a1
Johnson <subede.johnson@gmail.com>
2023-01-04 21:19:32 -0500

    pairwise and condorcet

--- project1/project1.py
+++ project1/project1.py
@@ -6,8 +6,43 @@ def simulate_election(num_voters, num_candidates):
     for i in range(num_voters):
         #generate random voters preferences
         preferences.append(random.sample(range(num_candidates), num_candidates))
+
+    # Determining the winner of each pairwise election
+    pairwise_winners = []
+    for i in range(num_candidates):
+        for j in range(i+1, num_candidates):
+            votes_for_i = 0
+            votes_for_j = 0
+            for k in range(num_voters):
+                if preferences[k][i] < preferences[k][j]:
+                    votes_for_i += 1
+                else: 
+                    votes_for_j += 1
+            if votes_for_i > votes_for_j:
+                pairwise_winners.append(i)
+            else: 
+                pairwise_winners.append(j)
+
+    # Checking if a condorcet winner exists
+    condorcet_winner = None
+    for i in range(num_candidates):
+        condorcet_winner_found = True
+        for j in range(num_candidates):
+            if i == j:
+                continue
+            if pairwise_winners[i*(num_candidates-1)+j] != i:
+                condorcet_winner_found = False
+                break
+        if condorcet_winner_found:
+            condorcet_winner = i
+            break
+            
+    return condorcet_winner
+        
+
+
     
-    return preferences
+
 
 
 '''def run_simulations(num_simulations, num_voters, num_candidates):
